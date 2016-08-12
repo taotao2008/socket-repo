180a2fdafe11c6a504db9ecac0e1a87b5c0fe824
taotao2008 <huangjuntao1987@126.com>
2016-08-12 16:26:23 +0800

    this is the second git

---
+++ server1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <unistd.h>
+/* 服务器要监听的本地端口 */
+#define MYPORT 4000
+/* 能够同时接受多少没有accept 的连接 */
+#define BACKLOG 10
+int main()
+{
+	int server_sockfd,client_sockfd;
+	int server_len,client_len;
+	struct sockaddr_un server_address;
+	struct sockaddr_un client_address;
+	unlink("server_socket");
+	server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+	server_address.sun_family = AF_UNIX;
+	strcpy(server_address.sun_path, "server_socket");
+	server_len = sizeof(server_address);
+	bind(server_sockfd, (struct sockaddr *) &server_address, server_len);
+	listen(server_sockfd, 5);
+	while(1) {
+		char ch;
+		printf("server waiting\n");
+		client_len = sizeof(client_address);
+		client_sockfd = accept(server_sockfd, (struct sockaddr *) &client_address, &client_len);
+		read(client_sockfd, &ch, 1);
+		ch++;
+		write(client_sockfd, &ch, 1);
+		close(client_sockfd);
+	}
+
+}
